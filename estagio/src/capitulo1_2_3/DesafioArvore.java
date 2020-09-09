96e8fff09e574e78ba88c517350621c84d3ee870
Alanpss <alanpatrick194@gmail.com>
2020-09-09 09:06:13 -0300

    Desafio

---
+++ estagio/src/capitulo1_2_3/DesafioArvore.java
@@ -0,0 +1,32 @@
+package capitulo1_2_3;
+
+import java.util.Scanner;
+
+public class DesafioArvore {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int asteristico = 1;
+		System.out.println("Digite o nomero de asteristico da base da arvore, sendo ele impar:");
+		int base = sc.nextInt();
+
+		if (base % 2 == 1) {
+			for (int linha = 1; linha <= base; linha++) {
+				for (int coluna = 1; coluna <= base; coluna++) { 
+					if (coluna == base) {
+						for (int i = 1; i <= asteristico; i++) {
+							System.out.print("*");
+						}
+						asteristico += 2;
+						base--;
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		}
+		sc.close();
+	}
+}
